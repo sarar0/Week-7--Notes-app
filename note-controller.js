dcad47caccd450e69c6c778957e7fefb42bf3904
Sara <srancati0@gmail.com>
2019-11-29 08:42:30 +0000

    working on step 6

--- note-controller.js
+++ note-controller.js
@@ -1,5 +1,27 @@
-var app = document.getElementById('app');
-console.log(app)
-console.log(app.innerHTML)
+// var app = document.getElementById('app');
+// console.log(app)
+// console.log(app.innerHTML)
 
-document.getElementById('app').innerHTML = "howdy"
+// document.getElementById('app').innerHTML = "howdy"
+
+
+(function(exports) {
+    
+    function NoteController(noteList) {
+        this.noteList = noteList;
+        this.noteList.createNote("hello Shubs")
+        this.notelistview = new NoteListView(this.noteList)
+    }
+
+    NoteController.prototype.changeHtml = function() {
+        document.getElementById('app').innerHTML = this.notelistview.displayHtml();
+    }
+
+    exports.NoteController = NoteController;
+}) (this)
+
+var notelist = new NoteList();
+
+var controller = new NoteController(notelist);
+
+controller.changeHtml()
