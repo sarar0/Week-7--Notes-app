c86283be3a9c9fb7875cdf9078063d94cb67665e
Sara <srancati0@gmail.com>
2019-12-01 11:26:07 +0000

    end of step 11 - submit event

--- public/src/note-controller.js
+++ public/src/note-controller.js
@@ -13,3 +13,49 @@
     exports.NoteController = NoteController;
 }) (this)
 
+// SUBMIT EVENT FOR NEW NOTE
+
+document.querySelector("#text").addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event)
+}, false);
+
+// CREATING NOTES:
+
+var notelist = new NoteList();
+var controller = new NoteController(notelist);
+
+notelist.createNote("first note first note first note first note first note first note")
+notelist.createNote("second note second note second note second note second note")
+notelist.createNote("third note third note third note third note third note")
+notelist.createNote("fourth note fourth note fourth note fourth note fourth note")
+
+controller.changeHtml()
+
+makeUrlChangeShowNote();
+
+      function makeUrlChangeShowNote() {
+        window.addEventListener("hashchange", showUniqueNote);
+      };
+
+      function showUniqueNote() {
+        showNote(getNoteFromUrl(window.location));
+      };
+
+      function getNoteFromUrl(location) {
+        var id_on_page = location.hash.split("#")[1];
+        return id_on_page
+      };
+
+      function showNote(id) {
+
+        var note = notelist.returnNotes()[id]
+        var singleNoteView = new SingleNoteView(note)
+
+
+        document
+          .getElementById("single-note")
+          .innerHTML = singleNoteView.returningHtml()
+      };
+
+   
